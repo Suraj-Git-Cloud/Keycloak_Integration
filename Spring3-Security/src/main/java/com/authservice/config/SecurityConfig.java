0c07f5f502a5321bac5f80bff448049f47a104b6
Suraj <surajgupta@idrive.com>
2025-01-08 12:26:30 +0530

    updated files

---
+++ Spring3-Security/src/main/java/com/authservice/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.authservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.authservice.service.UserInfoDetailService;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	public UserDetailsService userDetailsService() { // Authentication
+
+		/*UserDetails admin = User.withUsername("surya").password(passwordEncoder.encode("asd")).roles("ADMIN").build();
+
+		UserDetails user = User.withUsername("jeet").password(passwordEncoder.encode("asd")).roles("USER").build();
+
+		return new InMemoryUserDetailsManager(admin, user); // to keep user in In-Memory DB */
+		
+		return new UserInfoDetailService();
+
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/products/welcome","/products/new").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/products/**")
+                .authenticated().and().formLogin().and().build();
+    }
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() { // understand in detail
+		DaoAuthenticationProvider daoAuthencationProvider = new DaoAuthenticationProvider();
+		daoAuthencationProvider.setUserDetailsService(userDetailsService());
+		daoAuthencationProvider.setPasswordEncoder(passwordEncoder());
+		
+		return daoAuthencationProvider;
+		
+	}
+}
---
+++ Spring3-Security/src/main/java/com/authservice/model/UserInfo.java
@@ -0,0 +1,30 @@
+package com.authservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "USERINFO")
+public class UserInfo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String roles;
+
+}
